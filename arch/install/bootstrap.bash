99c751cb2850c34f787491b8682a868e711ae660
Ryoma Zhang <dev@zhanglei.me>
2024-04-05 09:53:48 +0800

    add bootstrap.bash

--- arch/install/bootstrap.bash
+++ arch/install/bootstrap.bash
@@ -34,8 +34,14 @@ if [[ $IS_UEFI == "1" ]]; then
     EFI_DEV=$(get_mount_fs /boot/efi)
 fi
 
-mkdir -p /tmp/5c44cf21-1004-4f76-8ee6-aec3f527aa0a
-cat << EOF > /tmp/5c44cf21-1004-4f76-8ee6-aec3f527aa0a/.env
+ARCHLINUX_BOOTSTRAP_URL=$(curl -Ls "https://archlinux.org/mirrorlist/?country=${LOC}&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on" | grep "Server" | sed 's|$repo/os/$arch|iso/latest/archlinux-bootstrap-x86_64.tar.gz|g' | awk '{print $3}' | head -n 1)
+curl -L "${ARCHLINUX_BOOTSTRAP_URL}" -O /archlinux-bootstrap-x86_64.tar.gz
+mkdir /install
+cd /install
+tar xzf /archlinux-bootstrap-x86_64.tar.gz --numeric-owner
+
+mkdir -p /install/root.x86_64/tmp/5c44cf21-1004-4f76-8ee6-aec3f527aa0a
+cat << EOF > /install/root.x86_64/tmp/5c44cf21-1004-4f76-8ee6-aec3f527aa0a/.env
 IS_UEFI=${IS_UEFI}
 ROOT_DEV=${ROOT_DEV}
 EFI_DEV=${EFI_DEV}
@@ -43,12 +49,5 @@ LOC=${LOC}
 IS_DHCP=${IS_DHCP}
 IS_HYPERV=${IS_HYPERV}
 EOF
-curl -Ls "https://github.com/echizenryoma/scripts/raw/main/arch/install/setup.bash" -o /tmp/5c44cf21-1004-4f76-8ee6-aec3f527aa0a/setup.bash
-
-ARCHLINUX_BOOTSTRAP_URL=$(curl -Ls "https://archlinux.org/mirrorlist/?country=${LOC}&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on" | grep "Server" | sed 's|$repo/os/$arch|iso/latest/archlinux-bootstrap-x86_64.tar.gz|g' | awk '{print $3}' | head -n 1)
-curl -L "${ARCHLINUX_BOOTSTRAP_URL}" -O /archlinux-bootstrap-x86_64.tar.gz
-mkdir /install
-cd /install
-tar xzf /archlinux-bootstrap-x86_64.tar.gz --numeric-owner
-echo "Server = https://${ArchMirrorDomain}/archlinux/\$repo/os/\$arch" >> /install/root.x86_64/etc/pacman.d/mirrorlist
+curl -Ls "https://github.com/echizenryoma/scripts/raw/main/arch/install/setup.bash" -o /install/root.x86_64/tmp/5c44cf21-1004-4f76-8ee6-aec3f527aa0a/setup.bash
 /install/root.x86_64/bin/arch-chroot /install/root.x86_64/ /tmp/5c44cf21-1004-4f76-8ee6-aec3f527aa0a/setup.bash
